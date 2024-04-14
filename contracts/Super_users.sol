443480db2ca43531d2e380e18d48ca3e6344045f
GrishaVoronin <voroningrisha2004@yandex.ru>
2024-04-14 22:28:47 +0300

    first version of super_users

--- contracts/Super_users.sol
+++ contracts/Super_users.sol
@@ -2,15 +2,15 @@
 pragma solidity ^0.8.0;
 
 contract SuperUsers {
-    mapping (address => bool) private is_super_user;
+    mapping (address => bool) private is_super_user_;
 
     constructor (address[] memory super_users) {
         for (uint i = 0; i < super_users.length; ++i) {
-            is_super_user[super_users[i]] = true;
+            is_super_user_[super_users[i]] = true;
         }
     }
 
     function isSuperUser(address user) view  public returns(bool) {
-        return is_super_user[user];
+        return is_super_user_[user];
     }
 }
