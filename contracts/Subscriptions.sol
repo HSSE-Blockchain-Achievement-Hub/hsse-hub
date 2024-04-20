dc7aa7427c78bc95672249ba8ee76a9db3bcfb1d
GrishaVoronin <voroningrisha2004@yandex.ru>
2024-04-20 12:17:20 +0300

    add voting version 2

--- contracts/Subscriptions.sol
+++ contracts/Subscriptions.sol
@@ -1,42 +1,55 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.0;
 
-
 contract Subscribers {
-  mapping (address => uint256) private subscribers_amount_;
-  mapping (address => mapping (address => bool)) private is_already_subscriber_;
+    mapping(address => uint256) private subscribers_amount_;
+    mapping(address => mapping(address => bool)) private is_already_subscriber_;
 
-  function isAlreadySubscriber(address from_, address to_) public view returns(bool) {
-    return is_already_subscriber_[from_][to_];
-  }
+    function isAlreadySubscriber(address from_, address to_)
+        public
+        view
+        returns (bool)
+    {
+        return is_already_subscriber_[from_][to_];
+    }
 
-  function isAlreadySubscriber(address to_) public view returns(bool) {
-    return is_already_subscriber_[msg.sender][to_];
-  }
+    function isAlreadySubscriber(address to_) public view returns (bool) {
+        return is_already_subscriber_[msg.sender][to_];
+    }
 
-  function safeSub(address from_, address to_) private {
-    is_already_subscriber_[from_][to_] = true;
-    ++subscribers_amount_[to_];
-  }
+    function safeSub(address from_, address to_) private {
+        is_already_subscriber_[from_][to_] = true;
+        ++subscribers_amount_[to_];
+    }
 
-  function safeUnsub(address from_, address to_) private {
-    is_already_subscriber_[from_][to_] = false;
-    --subscribers_amount_[to_];
-  }
+    function safeUnsub(address from_, address to_) private {
+        is_already_subscriber_[from_][to_] = false;
+        --subscribers_amount_[to_];
+    }
 
-  function subscribeOn(address to_) public {
-    require(!isAlreadySubscriber(msg.sender, to_), "You're already subscribed");
-    require(msg.sender != to_, "You can't subscribed on yourself!");
-    safeSub(msg.sender, to_);
-  }
+    function subscribeOn(address to_) public {
+        require(
+            !isAlreadySubscriber(msg.sender, to_),
+            "You're already subscribed"
+        );
+        require(msg.sender != to_, "You can't subscribed on yourself!");
+        safeSub(msg.sender, to_);
+    }
 
-  function unsubscribeFrom(address from_) public {
-    require(isAlreadySubscriber(msg.sender, from_), "You're haven't been subscribed");
-    require(msg.sender != from_, "You can't unsubscribe from yourself!");
-    safeUnsub(msg.sender, from_);
-  }
+    function unsubscribeFrom(address from_) public {
+        require(
+            isAlreadySubscriber(msg.sender, from_),
+            "You're haven't been subscribed"
+        );
+        require(msg.sender != from_, "You can't unsubscribe from yourself!");
+        safeUnsub(msg.sender, from_);
+    }
 
-  function getSubscribersAmount(address target_) view public returns(uint256) {
-    return subscribers_amount_[target_];
-  }
+    function getSubscribersAmount(address target_)
+        public
+        view
+        returns (uint256)
+    {
+        return subscribers_amount_[target_];
+    }
 }
--- contracts/SuperUsers.sol
+++ contracts/SuperUsers.sol
@@ -2,75 +2,102 @@
 pragma solidity ^0.8.0;
 
 contract SuperUsers {
-    mapping (address => bool) private is_super_user_;
+    mapping(address => bool) private is_super_user_;
 
-    constructor (address[] memory super_users) {
-        for (uint i = 0; i < super_users.length; ++i) {
+    constructor(address[] memory super_users) {
+        for (uint256 i = 0; i < super_users.length; ++i) {
             is_super_user_[super_users[i]] = true;
         }
     }
 
-    function isSuperUser(address user) view  public returns(bool) {
+    function isSuperUser(address user) public view returns (bool) {
         return is_super_user_[user];
     }
 
-    uint public constant voting_duration = 1 minutes;
-    mapping (uint => Voting) public votings_;
-    mapping (uint => mapping (address => bool)) has_voted_;
-    uint private current_voting_number_ = 0;
+    uint256 public constant voting_duration = 2 days;
+    mapping(uint256 => Voting) private votings_;
+    mapping(uint256 => mapping(address => bool)) has_voted_;
+    uint256 private current_voting_number_ = 0;
 
     struct Voting {
         address user;
-        uint againstVotes;
-        uint forVotes;
-        uint votingStarts;
+        uint256 againstVotes;
+        uint256 forVotes;
+        uint256 votingStarts;
         bool executed;
     }
 
     function setVoting(address user) public {
-        require(is_super_user_[user] == false, "This address's already a super user");
+        require(
+            is_super_user_[user] == false,
+            "This address's already a super user"
+        );
         ++current_voting_number_;
 
-        Voting memory newVoting = Voting(
-            user,
-            0,
-            0,
-            block.timestamp,
-            false
+        Voting memory newVoting = Voting(user, 0, 0, block.timestamp, false);
+
+        votings_[current_voting_number_] = newVoting;
+    }
+
+    modifier onlyUnvoted(uint256 voting_number) {
+        require(
+            has_voted_[voting_number][msg.sender] == false,
+            "You've already voted"
         );
-
-        votings_[current_voting_number_] = newVoting; 
-    }
-
-    modifier onlyUnvoted(uint voting_number) {
-        require(has_voted_[voting_number][msg.sender] == false, "You've already voted");
         _;
     }
 
-    modifier hasNotFinished(uint voting_number) {
-        require(votings_[voting_number].votingStarts + voting_duration > block.timestamp, "This voting's already finished");
+    modifier hasNotFinished(uint256 voting_number) {
+        require(
+            votings_[voting_number].votingStarts + voting_duration >
+                block.timestamp,
+            "This voting's already finished"
+        );
         _;
     }
 
-    modifier validVoting(uint voting_number) {
-        require(voting_number > 0 && voting_number <= current_voting_number_, "Invalid voting number");
+    modifier validVoting(uint256 voting_number) {
+        require(
+            voting_number > 0 && voting_number <= current_voting_number_,
+            "Invalid voting number"
+        );
         _;
     }
 
-    function voteFor(uint voting_number) public onlyUnvoted(voting_number) hasNotFinished(voting_number) validVoting(voting_number) {
+    function voteFor(uint256 voting_number)
+        public
+        onlyUnvoted(voting_number)
+        hasNotFinished(voting_number)
+        validVoting(voting_number)
+    {
         has_voted_[voting_number][msg.sender] = true;
         ++votings_[voting_number].forVotes;
     }
 
-    function voteAgainst(uint voting_number) public onlyUnvoted(voting_number) hasNotFinished(voting_number) validVoting(voting_number) {
+    function voteAgainst(uint256 voting_number)
+        public
+        onlyUnvoted(voting_number)
+        hasNotFinished(voting_number)
+        validVoting(voting_number)
+    {
         has_voted_[voting_number][msg.sender] = true;
         ++votings_[voting_number].againstVotes;
     }
 
-    function summarizeVoting(uint voting_number) public validVoting(voting_number) {
-        require(votings_[voting_number].votingStarts + voting_duration < block.timestamp, "Voting hasn't finished");
+    function summarizeVoting(uint256 voting_number)
+        public
+        validVoting(voting_number)
+    {
+        require(
+            votings_[voting_number].votingStarts + voting_duration <
+                block.timestamp,
+            "Voting hasn't finished"
+        );
         votings_[voting_number].executed = true;
-        if (votings_[voting_number].forVotes > votings_[voting_number].againstVotes) {
+        if (
+            votings_[voting_number].forVotes >
+            votings_[voting_number].againstVotes
+        ) {
             is_super_user_[votings_[voting_number].user] = true;
         }
     }
