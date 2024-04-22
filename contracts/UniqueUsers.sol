c4438a2bfc273ad9fb7d446b50b7a34880a98b24
GrishaVoronin <80212278+GrishaVoronin@users.noreply.github.com>
2024-04-22 15:39:06 +0300

    change to bool map

--- contracts/UniqueUsers.sol
+++ contracts/UniqueUsers.sol
@@ -4,13 +4,13 @@ pragma solidity ^0.8.0;
 
 contract UniqueUsers {
   uint256 private unique_users_cnt_ = 0;
-  mapping (address => uint256) private users_active_; 
+  mapping (address => bool) private users_active_; 
 
   function addCount(address user_) external  {
-    if (users_active_[user_] == 0) {
+    if (!users_active_[user_]) {
       ++unique_users_cnt_;
     }
-    ++users_active_[user_];
+    users_active_[user_] = true;
   }
 
   function getUniqueUsersCnt() public view returns(uint256) {
