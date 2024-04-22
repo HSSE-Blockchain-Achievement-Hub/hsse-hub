24bd36136dd283ace6f08cf2ce156968a011da67
droid55 <aleksian.de@phystech.edu>
2024-04-23 01:54:27 +0300

    made file for contracts creation

---
+++ back-end/contracts/Contracts.py
@@ -0,0 +1,9 @@
+# https://web3py.readthedocs.io/en/stable/web3.contract.html
+
+# Конструируем контракт ".constructor().transact()"
+# wait_for_transaction_receipt(tx_hash)
+
+# test_contract = w3.eth.contract(
+#     address=tx_receipt.contractAddress,
+#     abi=abi
+# )
