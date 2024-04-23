be68f7a6d6d1100bcd0d0c3f710ecafd3ed41be0
k33pup <nikoboga90@gmail.com>
2024-04-23 23:18:42 +0300

    add unique modifier to usernames

--- contracts/Usernames.sol
+++ contracts/Usernames.sol
@@ -6,6 +6,11 @@ contract Usernames {
   mapping (address => string) private usernames_;
   mapping (string => address) private owners_;
 
+  modifier isUnique(string memory username_) {
+    require(getOwner(username_) == address(0), "username is already taken!");
+    _;
+  }
+
   function hasUsername() public view returns (bool) {
     bytes memory bytestring_ = bytes(usernames_[msg.sender]);
     return bytestring_.length != 0;   
@@ -16,6 +21,10 @@ contract Usernames {
     return bytestring_.length != 0;   
   }
 
+  function getOwner(string memory username_) public view returns(address) {
+    return owners_[username_];
+  }
+
   function getUsername() public view returns (string memory) {
     return usernames_[msg.sender];
   }
@@ -24,7 +33,7 @@ contract Usernames {
     return usernames_[user_];
   }
 
-  function setUsername(string memory name_) external {
+  function setUsername(string memory name_) external isUnique(name_) {
     usernames_[msg.sender] = name_;
   }
 }
