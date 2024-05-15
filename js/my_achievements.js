7a81392a89eb953bdff7ffedf074e22889f112f5
Kazakov Arseniy Stanislavovich <144142778+Cra2er@users.noreply.github.com>
2024-05-15 20:45:41 +0300

    Create my_achievements.js

---
+++ js/my_achievements.js
@@ -0,0 +1,29 @@
+const addButton = document.querySelector('.input__btn_action_add');
+const form = document.forms.transfer;
+const name = form.elements.name;
+const isValid = true;
+
+function addTransfer(recieverValue) {
+
+}
+
+function setSubmitButtonState(isFormValid) {
+    if (isFormValid) {
+        addButton.removeAttribute('disabled');
+        addButton.classList.remove('input__btn_disabled');
+    } else {
+        addButton.setAttribute('disabled', true);
+        addButton.classList.add('input__btn_disabled');
+    }
+}
+
+form.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+    addTransfer(name.value);
+    name.value = '';
+});
+
+form.addEventListener('input', function (evt) {
+    const isValid = name.value.length > 0;
+    setSubmitButtonState(isValid);
+});
