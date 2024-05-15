5f5b7f83b1064995053962256557822bf56eadcb
Kazakov Arseniy Stanislavovich <144142778+Cra2er@users.noreply.github.com>
2024-05-16 01:54:35 +0300

    Update creation_form.js

--- js/creation_form.js
+++ js/creation_form.js
@@ -1,18 +1,18 @@
 const addButton = document.querySelector('.input__btn_action_add');
-const form = document.forms.create;
-const name = form.elements.name;
-const key = form.elements.key;
-const ref = form.elements.ref;
-const descr = form.elements.descr;
-const checked = form.elements.checked;
-const isValid = true;
+const creation_form = document.forms.create;
+const name = creation_form.elements.name;
+const key = creation_form.elements.key;
+const ref = creation_form.elements.ref;
+const descr = creation_form.elements.descr;
+const checked = creation_form.elements.checked;
+const isValid = false;
 
 // Заглушка для бэкенда
 function addCreation(nameValue, keyValue, refValue, descrValue, checkedValue) {
 
 }
 
-function setSubmitButtonState(isFormValid) {
+function setSubmitButtonState_creation(isFormValid) {
     if (isFormValid) {
         addButton.removeAttribute('disabled');
         addButton.classList.remove('input__btn_disabled');
@@ -22,10 +22,10 @@ function setSubmitButtonState(isFormValid) {
     }
 }
 
-form.addEventListener('submit', function (evt) {
+creation_form.addEventListener('submit', function (evt) {
     evt.preventDefault();
     addCreation(name.value, key.value, ref.value, descr.value, checked.value);
-    setSubmitButtonState(false);
+    setSubmitButtonState_creation(false);
     name.value = '';
     key.value = '';
     ref.value = '';
@@ -33,7 +33,7 @@ form.addEventListener('submit', function (evt) {
     checked.checked = false;
 });
 
-form.addEventListener('input', function (evt) {
+creation_form.addEventListener('input', function (evt) {
     const isValid = name.value.length > 0 && ((key.value[0] === '@' && key.value.length > 1) || key.value.length === 42) && key.value.length > 0 && ref.value.length > 0;
-    setSubmitButtonState(isValid);
+    setSubmitButtonState_creation(isValid);
 });
