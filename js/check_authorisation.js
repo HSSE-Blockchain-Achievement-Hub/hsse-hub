c156c61328723daa75ff5007555e6b7afa4a2f65
Kazakov Arseniy Stanislavovich <144142778+Cra2er@users.noreply.github.com>
2024-05-16 16:44:05 +0300

    Update check_authorisation.js

--- js/check_authorisation.js
+++ js/check_authorisation.js
@@ -14,20 +14,22 @@ async function checkAutho() {
         menuWallet.style.display = 'flex';
         let address = getCookie("address");
         let balance = getCookie("balance");
+        address = '0xkeognweoiniowejiofmweopgmfiogdfgfdgfdgdfgwengiwe';
+        balance = 0.00001;
         walletAddress.innerHTML = address.substr(0, 5) + '...' + address.substr(39, 3);
         walletBalance.innerHTML = balance + ' ETH';
-
         menuSignUp.classList.add('menu__item_able');
         menuSignUp.innerHTML = 'Выйти';
+
     } else {
         menuWallet.style.display = 'none';
         walletAddress.innerHTML = '';
         walletBalance.innerHTML = '';
 
         menuSignUp.classList.remove('menu__item_able');
+        menuSignUp.innerHTML = 'Авторизация';
     }
 }
-
 
 document.cookie = "initialization=true; secure";
 document.cookie = "account=" + "0xadsioajfguierhgtioewrnjgifowearsegefhedfherhdsfzhdfsgjrthsrthsre" + "; secure";
