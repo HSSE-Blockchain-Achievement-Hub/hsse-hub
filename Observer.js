6fdb3e396efd011a66f43fde841b7beba8adfd85
Liiizak <144727441+Liiizak@users.noreply.github.com>
2024-05-19 01:16:30 +0300

    Update Observer.js

--- Observer.js
+++ Observer.js
@@ -1,14 +1,14 @@
 class Observer {
-    get_all() {
-        let account = getCookie('account');
+    async get_all() {
+        let account = await getCookie('account');
         let all_achievements = [];
         contract_myToken.methods.getAllAchievements(account).call().then(item => all_achievements.push(item[0]))
         console.log(all_achievements)
         return all_achievements
     }
 
-    get_open() {
-        let account = getCookie('account');
+    async get_open() {
+        let account = await getCookie('account');
         let open_achievements = []
         contract_myToken.methods.getAllAchievements(account).call().then(item => open_achievements.push(item[0]))
         open_achievements.filter(item => item.is_private)
