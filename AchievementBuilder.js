56fae4c156c1344b6684080fc2a56688eeb4387a
Liiiza <el1zarudneva@yandex.ru>
2024-05-19 01:32:21 +0300

    fixed variable

--- AchievementBuilder.js
+++ AchievementBuilder.js
@@ -37,13 +37,13 @@ async build() {
         'value': 0,
         'gas': 100000000,
         'chainId': await web3.eth.getChainId().then(count => parseInt(count, 10)),
-        'from': account,
+        'from': await getCookie('account'),
         'gas_price': await web3.eth.getGasPrice().then(count => parseInt(count, 10)),
-        'nonce': await web3.eth.getTransactionCount(account, 'pending').then(count => parseInt(count, 10)),
+        'nonce': await web3.eth.getTransactionCount(await getCookie('account'), 'pending').then(count => parseInt(count, 10)),
         'to': my_token_address,
     }
     if (my_token_contract) {
-        await my_token_contract.methods.mint(this.name, this.description, this.baseURL, this.isPrivate, account).send(achievement_dict_transaction)
+        await my_token_contract.methods.mint(this.name, this.description, this.baseURL, this.isPrivate, await getCookie('account')).send(achievement_dict_transaction)
             .catch(error => {
                 console.log(error)
             })
